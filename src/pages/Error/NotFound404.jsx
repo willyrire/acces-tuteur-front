97ce3e1562049bc63ebed00db158405d97916359
willyrire <leveillew7@gmail.com>
2026-01-20 18:31:59 -0500

    Ajout de la page de connexion et ajout de la page d'erreur 404.

---
+++ src/pages/Error/NotFound404.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Header from "../../components/Header/Header";
+function NotFound404(){
+    return (
+        <Header isAuth={false} minimalist={true} />
+    )
+}
+
+export default NotFound404;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,13 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "../pages/Homepage";
 import LoginPage from "../pages/Auth/LoginPage";
+import NotFound404 from "../pages/Error/NotFound404";
 // Ici tu pourras importer LoginPage, DashboardParent, DashboardTutor, etc.
 
 const AppRoutes = () => {
+    const isAuth = false; // Remplace ceci par ta logique d'authentification réelle
+    const userName = "L. William"; // Remplace ceci par le nom d'utilisateur réel si nécessaire
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<HomePage />} />
+                <Route path="/" element={<HomePage isAuth={isAuth} userName={userName} />} />
                 <Route path="/auth/login" element={<LoginPage />} />
 
                 
@@ -17,7 +20,7 @@ const AppRoutes = () => {
                 {/* <Route path="/login" element={<LoginPage />} /> */}
                 
                 {/* Fallback 404 */}
-                <Route path="*" element={<div>Page not found</div>} />
+                <Route path="*" element={<NotFound404 />} />
             </Routes>
         </Router>
     );

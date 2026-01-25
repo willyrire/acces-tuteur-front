a3fbbed291c71729c7769759481a0a77e204a82d
willyrire <leveillew7@gmail.com>
2026-01-25 14:00:16 -0500

    Page d'erreur terminée.

--- src/pages/Error/NotFound404.jsx
+++ src/pages/Error/NotFound404.jsx
@@ -2,28 +2,39 @@ import React from "react";
 import Section from "@/components/Section";
 import Header from "@/components/Header/Header";
 import Picture from "@/components/ui/picture";
+import Footer from "@/components/Footer";
 
 function NotFound404({ isAuth, userName }) {
   return (
     <div>
       <Header isAuth={isAuth} userName={userName} />
       <Section
-      title= "404 - Page Not Found"
-      children={
-        <>
-        <p className="mt-4 text-gray-600">
-          Désolé, la page que vous recherchez n'existe pas.
-        </p>
-          <Picture
-            source="/images/404-error-page-robot.png"
-            alt="404 Not Found"
-            className="w-80 md:w-96 h-auto opacity-95"
-          />
-        </>
-      }
-      className="bg-white min-h-[70vh] flex flex-col justify-center items-center text-center mt-[5%]"
-      titleAlignement="text-center"
+        title="404 - Page introuvable"
+        children={
+          <>
+            <p className="mt-4 text-gray-600">
+              Désolé, la page que vous tentez d'accéder n'existe plus ou a été
+              changée de place.
+            </p>
+            <Picture
+              source="/images/404-error-page-robot.png"
+              alt="404 Not Found"
+              className="w-80 md:w-96 h-auto opacity-95"
+            />
+            <p className="mt-4">
+              <a
+                href="/"
+                className="items-center rounded-full hover:text-black transition hover:bg-white p-4 border-2 border-blue-500 bg-blue-500 text-white font-bold align-center"
+              >
+                Retour à l'accueil
+              </a>
+            </p>
+          </>
+        }
+        className="bg-white mb-30 min-h-[70vh] flex flex-col justify-center items-center text-center mt-[5%]"
+        titleAlignement="text-center"
       />
+      <Footer />
     </div>
   );
 }

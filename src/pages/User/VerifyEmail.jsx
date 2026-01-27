1383baeb766bad021bdfbecae3208977b85d4f31
willyrire <leveillew7@gmail.com>
2026-01-27 14:44:33 -0500

    Ajout d'un bloqueur de loop dans VerifyEmail

--- src/pages/User/VerifyEmail.jsx
+++ src/pages/User/VerifyEmail.jsx
@@ -1,17 +1,30 @@
-import React from "react";
-
+import { React, useState } from "react";
+import getParams from "@/utils/tools/getParams";
+import { isEmpty } from "@/utils/tools/isEmpty";
 import Header from "@/components/Header/Header";
 import { div } from "framer-motion/client";
 import Footer from "@/components/Footer";
-function Profile({isAuth, userName}) {
+
+function Profile({ isAuth, userName }) {
+  const [hasParam, setHasParam] = useState(false);
+  const [hasInterpreted, setHasInterpreted] = useState(false);
+  const param = getParams();
+    if (!isEmpty(param.code)){
+        setHasParam(true);
+        setHasInterpreted(true);
+    }
   return (
     <div>
-        {/* Navigation */}
-        <Header removeWarnings={true} isAuth={isAuth} userName={userName} />
-        {/* Contenue */}
-        
-        {/* Footer */}
-        <Footer />
+      {/* Navigation */}
+      <Header removeWarnings={true} isAuth={isAuth} userName={userName} />
+      {/* Contenue */}
+      {hasParam ? (
+        <></>
+      ) : (
+        <></>
+      )}
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }

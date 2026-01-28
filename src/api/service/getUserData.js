e977b69fb274a92c2163e8bb7cf583446ad4b81d
willyrire <leveillew7@gmail.com>
2026-01-27 19:33:33 -0500

    Correction d'une faute du nom de l'item dans getUserData. Ça essayait de get user depuis de stockage local plutôt que userId...lol

--- src/api/service/getUserData.js
+++ src/api/service/getUserData.js
@@ -1,7 +1,7 @@
 import API from '../client';
 
 export default async function getUserData() {
-    const user = localStorage.getItem('user') ?? 67; // self
+    const user = localStorage.getItem('userId') ?? 67; // self
     const response = await API.get('/v1/user/'+user);
     return response.data;
 }

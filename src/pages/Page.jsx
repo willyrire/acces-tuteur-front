6d5ede49b41f07aa86d7420d0f88aea50373f2b6
willyrire <leveillew7@gmail.com>
2026-02-19 16:50:28 -0500

    Ajout du robot erreur 404pour une page non trouvée.

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -3,8 +3,10 @@ import Header from "@/components/Header/Header";
 import Section from "@/components/Section";
 import Footer from "@/components/Footer";
 import { getPage } from "@/api/public/getPage";
+import Picture from "@/components/ui/picture";
 import { useParams } from "react-router-dom";
 import MdPreview from "@/components/Preview/MdPreview";
+import { Loader2 } from "lucide-react";
 
 function Page({ isAuth, userName }) {
   const { slug } = useParams();
@@ -60,22 +62,48 @@ function Page({ isAuth, userName }) {
 
       {status === "loading" && (
         <Section
-          title="Chargement..."
-          className="bg-white-500 pb-30 pt-50 max-w-4xl mx-auto text-justify"
+          className="bg-white-500 mb-93 pb-30 pt-50 max-w-4xl mx-auto text-justify"
           titleAlignement="text-center"
         >
-          On charge la page.
+          <div className="flex items-center justify-center gap-3">
+            <Loader2 className="h-6 w-6 animate-spin" />
+            <span>Chargement en cours ...</span>
+            <br />
+          </div>
         </Section>
       )}
 
       {status === "error" && (
         <Section
-          title="Erreur"
-          className="bg-white-500 pb-20 pt-50 max-w-4xl mx-auto text-justify"
+          title="404 - Page introuvable"
+          children={
+            <>
+              <p className="mt-4 text-gray-600 font-semibold">
+                Désolé, la page que vous tentez d'accéder n'existe plus ou a été
+                changée de place. <br />
+                Accès Tuteur est un projet en développement constant. <br />
+                Il est donc possible que cette page finisse par être ajoutée (ou
+                pas) ! <br />
+                Vous le trouvez comment le robot?
+              </p>
+              <Picture
+                source="/images/404-error-page-robot.png"
+                alt="404 Not Found"
+                className="w-80 md:w-96 h-auto opacity-95"
+              />
+              <p className="mt-4">
+                <a
+                  href="/"
+                  className="items-center rounded-full hover:text-black transition hover:bg-white p-4 border-2 border-blue-500 bg-blue-500 text-white font-bold align-center"
+                >
+                  Retour à l'accueil
+                </a>
+              </p>
+            </>
+          }
+          className="bg-white mb-30 min-h-[70vh] flex flex-col justify-center items-center text-center mt-25"
           titleAlignement="text-center"
-        >
-          Page introuvable ou erreur de chargement.
-        </Section>
+        />
       )}
 
       {status === "success" && (
